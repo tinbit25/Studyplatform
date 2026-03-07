98d382f0483356ab85ae2e47a960d5bed79e5222
tinbit25 <tinbitelias25@gmail.com>
2026-03-07 14:44:15 +0300

    edit readme and adjust route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { AppController } from './modules/app/app.controller';
+import { AppService } from './modules/app/app.service';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

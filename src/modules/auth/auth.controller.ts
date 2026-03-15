28c830b2f6556f4a3b93b4ce65951050e2865d25
tinbit25 <tinbitelias25@gmail.com>
2026-03-15 20:49:49 +0300

    add profile page

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { UseGuards, Get, Request } from '@nestjs/common';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
- @Post("register")
-async register(@Body() body: any) {
-  return this.authService.register(body);
+ @Post('register')
+register(@Body() data: RegisterDto) {
+  return this.authService.register(data);
 }
-@Post("login")
-async login(@Body() body: any) {
-  return this.authService.login(body);
+
+@Post('login')
+login(@Body() data: LoginDto) {
+  return this.authService.login(data);
+}
+@UseGuards(JwtAuthGuard)
+@Get('profile')
+getProfile(@Request() req) {
+  return {
+    message: "Profile fetched successfully",
+    user: req.user
+  };
 }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 import { User, UserSchema } from './schemas/user.schema';
 
@@ -20,6 +21,6 @@ import { User, UserSchema } from './schemas/user.schema';
   ],
 
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
